f61452e29f14db0de3dba7995b26d491544c8dad
santen8a <sangaytenzin8a@gmail.com>
2022-04-16 08:30:18 +0600

    added a loop to continuously ask the user until the right command is typed in for program continuation

--- pokeApi.py
+++ pokeApi.py
@@ -9,7 +9,7 @@ while isRunning:
     req = requests.get(url)
 
     if req.status_code == 200:
-        #PokeInfo to json
+        # PokeInfo to json
         pokeData = req.json()
         # print(pokeData)
         print(f"Name \t {pokeData['name']}")
@@ -17,18 +17,23 @@ while isRunning:
 
         print("Abilities")
         for ability in pokeData['abilities']:
-            print("\t",ability['ability']['name'])
+            print("\t", ability['ability']['name'])
     elif req.status_code == 404:
         print("Error, Pokemon not found")
     else:
         print("Error: Please contact admin")
-   
-    continueQuestion = input("Do you wish to search for more info? (y/n)")
-    if(continueQuestion == 'n'):
-        isRunning = False
-    elif(continueQuestion == 'y'):
-        isRunning = True
-    else:
-        print("Input not understood, moving to main menu")
+
+    userInputAcceptable = False
+    while not userInputAcceptable:
+        continueQuestion = input("Do you wish to search for more info? (y/n)").lower()
+       
+        if(continueQuestion == 'n'):
+            userInputAcceptable = True
+            isRunning = False
+        elif(continueQuestion == 'y'):
+            userInputAcceptable = True
+            isRunning = True
+        else:
+            print("Input not understood, Please Enter Again")
 
 print("Bye Bye, see you again!!!")
